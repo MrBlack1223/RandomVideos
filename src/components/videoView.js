5839ff8743d4aab3ba7fda5b810a765539137c62
Wiktor Tarnowski <MrBlack1223@wp.pl>
2023-05-07 04:01:40 +0200

    Adding validation and trending videos also cleaning code a bit

--- src/components/videoView.js
+++ src/components/videoView.js
@@ -91,7 +91,7 @@ const View = ()=>{
                         <SubscribeButton />
                     </div>
                     <div className = {hide ? "hiddenVideoDesc" : "fullVideoDesc"}>
-                        {currentVideo.createdAt && `${currentVideo.createdAt}`}
+                        {currentVideo.createdAt && `${currentVideo.createdAt.getFullYear()}/${currentVideo.createdAt.getMonth()}/${currentVideo.createdAt.getDate()}`}
                         {currentVideo.desc}
                     </div>
                 </div>
